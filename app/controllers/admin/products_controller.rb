0d79c40193c9f1322646c86ad51aad6a2ad1e852
Carol Hsu <sheseee@gmail.com>
2012-07-19 18:28:17 +0800

    fixed the bug of admin part

--- app/controllers/admin/products_controller.rb
+++ app/controllers/admin/products_controller.rb
@@ -5,13 +5,12 @@ class Admin::ProductsController < ApplicationController
   layout "admin"
 
   def index
-    if params[:country_id].present?
-      @products = Product.get_country(params[:country_id]).paginate(:page => params[:page], :per_page => 10)
-    elsif params[:discount].present?
+    if params[:discount].present?
       @products = Product.find_by_sql("select * from products order by discount limit 1").paginate(:page => params[:page], :per_page => 10)
     else
-      @products = Product.available.paginate(:page => params[:page], :per_page => 10)
+      @products = Product.paginate(:page => params[:page], :per_page => 10)
     end
+
   end
 
   def new
